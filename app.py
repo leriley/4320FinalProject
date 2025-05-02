6237526341896fac0f9d41ea40708191984f6ffa
Lenore Riley <rileyle@umsystem.edu>
2025-05-01 23:07:42 -0500

    Set up initial page routing/base html and css

--- app.py
+++ app.py
@@ -2,20 +2,24 @@ from flask import Flask, render_template, request, redirect, url_for, Blueprint,
 from flask_sqlalchemy import SQLAlchemy
 import os
 
-#create a blueprint for the application
-reservation_bp = Blueprint('tasks', __name__)
-
+app = Flask(__name__)
 #Index page view/route
-@reservation_bp.route('/', methods=['GET'])
+#'app' on these routes will need to be updated when the database is set up so the blueprints can be incorperated
+#from @app.route to @[blueprint_name].bp.route
+@app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
 
 #Admin page view/route
-@reservation_bp.route('/admin', methods=['GET'])
-def index():
+@app.route('/admin', methods=['GET'])
+def admin():
     return render_template('admin.html')
 
 #Reservations page view/route
-@reservation_bp.route('/reservations', methods=['GET'])
-def index():
+@app.route('/reserve', methods=['GET'])
+def reserve():
     return render_template('reserve.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5019)
